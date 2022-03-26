7b3c60d911ecd5655538abe5a05d31519c5f2f09
Lukas Deutz <github@deutz.io>
2022-03-26 15:23:18 -0400

    Add starship prompt

---
+++ zsh/dot-zsh.d/alias.zsh
@@ -0,0 +1,9 @@
+alias ls='exa'
+alias cat='bat'
+alias grep='echo Try using rg'
+alias find='echo Try using fd'
+
+alias readme='glow -p README.md'
+alias vale-default='vale --config=$HOME/.vale.ini'
+
+alias gdc='git diff --cached'
