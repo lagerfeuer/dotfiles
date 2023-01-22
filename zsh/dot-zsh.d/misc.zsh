f97563d97e17c03f4e4bb7a75bf210e8583a26a4
Lukas Deutz <github@deutz.io>
2023-01-22 11:32:48 -0500

    Fix valgrind error with debuginfod

---
+++ zsh/dot-zsh.d/misc.zsh
@@ -0,0 +1,3 @@
+# fix valgrind debuginfod
+# see: https://www.reddit.com/r/archlinux/comments/vdu47o/valgrind_fatal_error_at_startup_a_function/
+export DEBUGINFOD_URLS="https://debuginfod.archlinux.org"
