f2678d0542c85bba8a683eae20caf14221dae54f
Lukas Deutz <github@deutz.io>
2024-02-19 12:28:07 -0500

    Add aliases

--- zsh/dot-zsh.d/alias.zsh
+++ zsh/dot-zsh.d/alias.zsh
@@ -1,4 +1,4 @@
-alias ls='exa'
+alias ls='eza'
 alias cat='bat'
 # alias grep='echo Try using rg'
 # alias find='echo Try using fd'
@@ -9,3 +9,6 @@ alias vale-default='vale --config=$HOME/.vale.ini'
 alias gdc='git diff --cached'
 
 alias doom='~/.emacs.d/bin/doom'
+
+alias doco='docker compose'
+
